3f45707c1fcdafffbf0a1b231c91daead578b261
francieli.sauthier <francieli.sauthier@gmail.com>
2023-10-09 12:52:47 +0200

    get all questions

--- src/api-calls.js
+++ src/api-calls.js
@@ -1,15 +1,32 @@
 import axios from 'axios';
 
-const baseURL = 'https://private-bbbe9-blissrecruitmentapi.apiary-mock.com/'
+const baseURL = 'https://private-bbbe9-blissrecruitmentapi.apiary-mock.com/';
 
 export const getHealthStatus = async () => {
-    try {
-      const response = await axios.get(`${baseURL}/health`);
-      if (response.status === 200) {
-        return response.data.status;
-      }
-    } catch (error) {
-      console.log(error);
+  try {
+    const response = await axios.get(`${baseURL}/health`);
+    if (response.status === 200) {
+      return response.data.status;
     }
-  };
+  } catch (error) {
+    console.log(error);
+  }
+};
 
+// to change later
+let limit = 10;
+let offset = limit;
+let filter = '';
+
+export const getQuestions = async () => {
+  try {
+    const response = await axios.get(
+      `${baseURL}/questions?limit=${limit}&offset=${offset}&filter=${filter}`
+    );
+    if (response.status === 200) {
+      return response.data;
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
