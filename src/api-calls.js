b5be1a8350057fe7b993939be64efb813ef12115
francieli.sauthier <francieli.sauthier@gmail.com>
2023-10-17 15:05:57 +0200

    get and render question details selected, refactor api function call

--- src/api-calls.js
+++ src/api-calls.js
@@ -31,15 +31,14 @@ export const getQuestions = async () => {
   }
 };
 
-//to change later - this call always returns question_id: 1
-const question_id = 5;
+//this call always returns question_id: 1
 
-export const getOneQuestionDetails = async () => {
+export const getOneQuestionDetails = async (QUESTION_ID) => {
+  console.log('QUESTION_ID FROM DETAIL SCREEN CALL:', QUESTION_ID);
   try {
-    const response = await axios.get(`${baseURL}/questions/${question_id}`);
+    const response = await axios.get(`${baseURL}/questions/${QUESTION_ID}`);
     if (response.status === 200) {
-      console.log('QUESTION ID', question_id);
-      console.log('QUESTION DETAIL', response.data);
+      console.log('QUESTION DETAILS ALWAYS RETURNS ID 1:', response.data);
       return response.data;
     }
   } catch (error) {
@@ -48,11 +47,14 @@ export const getOneQuestionDetails = async () => {
 };
 
 // to change later - this call always returns question_id: 1 and it does not really update a db
-const body = {}
+const body = {};
 
 export const updateQuestion = async () => {
   try {
-    const response = await axios.put(`${baseURL}/questions/${question_id}`, body);
+    const response = await axios.put(
+      `${baseURL}/questions/${question_id}`,
+      body
+    );
     if (response.status === 201) {
       console.log('UPDATE', body);
       console.log('UPDATE', response.data);
@@ -64,12 +66,14 @@ export const updateQuestion = async () => {
 };
 
 // to change later
-let destination_email = 'test@test.com'
-let content_url = 'pageURL'
+let destination_email = 'test@test.com';
+let content_url = 'pageURL';
 
 export const shareQuestion = async () => {
   try {
-    const response = await axios.post(`${baseURL}/share?destination_email=${destination_email}&content_url=${content_url}`);
+    const response = await axios.post(
+      `${baseURL}/share?destination_email=${destination_email}&content_url=${content_url}`
+    );
     if (response.status === 200) {
       console.log('RESPONSE', response.data.status);
       return response.data.status;
--- src/pages/DetailScreen.jsx
+++ src/pages/DetailScreen.jsx
@@ -1,5 +1,38 @@
+import { useEffect, useState } from 'react';
+import { getOneQuestionDetails } from '../api-calls';
+import { useParams } from 'react-router';
+
 export default function DetailScreen() {
-  return (
-    <div>DetailScreen</div>
-  )
+  const [questionDetails, setQuestionDetails] = useState();
+  const { QUESTION_ID } = useParams();
+  console.log('QUESTION_ID FROM USER CLICK:', QUESTION_ID);
+
+  useEffect(() => {
+    const getData = async () => {
+      const data = await getOneQuestionDetails(QUESTION_ID);
+      setQuestionDetails(data);
+    };
+    getData();
+  }, [QUESTION_ID]);
+
+  return questionDetails ? (
+    <div>
+      <img
+        src={questionDetails.image_url}
+        alt='picture related to question content'
+      />
+      <h1>Question: {questionDetails.question}</h1>
+      <p>Published: {questionDetails.published_at}</p>
+      <ul>
+        {questionDetails.choices.map((choice) => (
+          <li key={choice.choice}>
+            {choice.choice} - {choice.votes} votes
+            <button type='button'> Vote </button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  ) : (
+    <div>Loading Screen...</div>
+  );
 }
